0156ac3c31b0214d25fc612afbee3f319ee5d7ea
Indranil Sen <indranilsen217@gmail.com>
2023-08-14 20:23:05 +0530

    Added logic for Login and Sign Up

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -8,19 +11,41 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  final form = GlobalKey<FormState>();
+  final _form = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
 
   var _isLogin = true;
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit() {
-    final isValid = form.currentState!.validate();
+  void _submit() async {
+    final isValid = _form.currentState!.validate();
 
-    if (isValid) {
-      form.currentState!.save();
-      print(_enteredEmail);
-      print(_enteredPassword);
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+
+    try {
+      if (_isLogin) {
+        await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      } else {
+        await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      }
+    } on FirebaseAuthException catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            _isLogin
+                ? 'Incorrect email address or password.'
+                : error.message ?? 'Authentication failed.',
+          ),
+          duration: const Duration(seconds: 3),
+        ),
+      );
     }
   }
 
@@ -58,6 +83,7 @@ class _AuthScreenState extends State<AuthScreen> {
               prefixIcon: const Icon(Icons.key),
             ),
             obscureText: true,
+            controller: _passwordController,
             validator: (value) {
               if (value == null || value.isEmpty || value.length < 6) {
                 return 'Password must have a length of 6.';
@@ -81,7 +107,7 @@ class _AuthScreenState extends State<AuthScreen> {
             validator: (value) {
               if (value == null ||
                   value.trim().isEmpty ||
-                  value != _enteredPassword) {
+                  value != _passwordController.text) {
                 return 'Confirm password must match with entered password';
               }
               return null;
@@ -120,7 +146,7 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Padding(
                   padding: const EdgeInsets.all(20),
                   child: Form(
-                    key: form,
+                    key: _form,
                     child: Column(
                       children: [
                         TextFormField(
